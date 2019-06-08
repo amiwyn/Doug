a40f3fc0caa6d9b79a9f50e9edfc1f0e60d41262
Hugues Trottier <hugues_trottier@hotmail.com>
2019-06-08 11:32:35 -0400

    util tests

---
+++ Test/UtilsTest.cs
@@ -0,0 +1,24 @@
+﻿using Doug;
+using Doug.Repositories;
+using Doug.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class UtilsTest
+    {
+        private const string User = "robert";
+
+        [TestMethod]
+        public void WhenMakingUserMention_ThenSlackDecorationIsAdded()
+        {
+            var mention = Utils.UserMention(User);
+
+            Assert.AreEqual("<@robert>", mention);
+        }
+
+    }
+}
