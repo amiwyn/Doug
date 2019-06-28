9e417d3c2716ba7ccc48bebc6ccd4c353c16f6b4
Hugues Trottier <hugues_trottier@hotmail.com>
2019-06-27 20:50:56 -0400

    fixed item stat influence + balanced steal amount

--- Doug/Models/User.cs
+++ Doug/Models/User.cs
@@ -8,6 +8,11 @@ namespace Doug.Models
     {
         private int _health;
         private int _energy;
+        private int _luck;
+        private int _agility;
+        private int _charisma;
+        private int _constitution;
+        private int _stamina;
 
         public string Id { get; set; }
         public int Credits { get; set; }
@@ -57,11 +62,35 @@ namespace Doug.Models
             }
         }
 
-        public int Luck { get; set; }
-        public int Agility { get; set; }
-        public int Charisma { get; set; }
-        public int Constitution { get; set; }
-        public int Stamina { get; set; }
+        public int Luck
+        {
+            get => Loadout.Luck + _luck;
+            set => _luck = value;
+        }
+
+        public int Agility
+        {
+            get => Loadout.Agility + _agility;
+            set => _agility = value;
+        }
+
+        public int Charisma
+        {
+            get => Loadout.Charisma + _charisma;
+            set => _charisma = value;
+        }
+
+        public int Constitution
+        {
+            get => Loadout.Constitution + _constitution;
+            set => _constitution = value;
+        }
+
+        public int Stamina
+        {
+            get => Loadout.Stamina + _stamina;
+            set => _stamina = value;
+        }
 
         public int Level => (int)Math.Floor(Math.Sqrt(Experience) * 0.1 + 1);
         public int TotalStatsPoints => (int)Math.Floor(Level + 5 * Math.Floor(Level * 0.1)) + 4;
@@ -106,31 +135,6 @@ namespace Doug.Models
             return energyFromLevel + energyFromStamina;
         }
 
-        public int TotalLuck()
-        {
-            return Loadout.Luck + Luck;
-        }
-
-        public int TotalAgility()
-        {
-            return Loadout.Agility + Agility;
-        }
-
-        public int TotalCharisma()
-        {
-            return Loadout.Charisma + Charisma;
-        }
-
-        public int TotalConstitution()
-        {
-            return Loadout.Constitution + Constitution;
-        }
-
-        public int TotalStamina()
-        {
-            return Loadout.Stamina + Stamina;
-        }
-
         public double BaseGambleChance()
         {
             var luckInfluence = Math.Log(Luck / 5.0) / (Math.Log(2) * 100);
@@ -150,7 +154,7 @@ namespace Doug.Models
 
         public int BaseStealAmount()
         {
-            return (int)Math.Floor(6 * (Math.Sqrt(Agility) - Math.Sqrt(5)) + 1);
+            return (int)Math.Floor(3 * (Math.Sqrt(Agility) - Math.Sqrt(5)) + 1);
         }
 
         public bool HasEnoughCreditsForAmount(int amount)
