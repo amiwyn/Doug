8dcea427988c66f4b7f8b55fa2ceab8568851178
WGF <girouxfortinw@hotmail.com>
2019-07-03 19:36:13 -0400

    just the items
    need to implement virtual methods and IDs for final functionality

---
+++ Doug/Items/Equipment/IncognitoShades.cs
@@ -0,0 +1,23 @@
+﻿using Doug.models;
+
+namespace Doug.Items.Equipment
+{
+    public class IncognitoShades : EquipmentItem
+    {
+        public IncognitoShades()
+        {
+            Id = ItemFactory.IncognitoShades;
+            Name = "Incognito Shades";
+            Description = "A pair of cool-looking sunglasses. Doug won't be able to recognize you while you wear these.";
+            Rarity = Rarity.Unique;
+            Icon = ":dark_sunglasses:";
+            Slot = EquipmentSlot.Head;
+            Price = 1337;
+        }
+
+        public override string OnMentioned(string userId)
+        {
+            return ":sunglasses:";
+        }
+    }
+}
---
+++ Doug/Items/Equipment/MainGauche.cs
@@ -0,0 +1,29 @@
+﻿namespace Doug.Items.Equipment
+{
+    public class MainGauche : EquipmentItem
+    {
+        public MainGauche()
+        {
+            Id = ItemFactory.MainGauche;
+            Name = "Main Gauche";
+            Description = "A dented, dulled brass shank. You won't be caught while /stealing, but any player that kills you steals this item.";
+            Rarity = Rarity.Unique;
+            Icon = ":dagger_knife:";
+            Slot = EquipmentSlot.LeftHand;
+            Price = 1000;
+
+            Attack = 9;
+            Agility = 2;
+        }
+
+        public override DougResponse OnStealingFailed(User user, string targetUserMention)
+        {
+            return "You failed to rob " + targetUserMention + ", but at least you didn't get caught... this time.";
+        }
+
+        public override DougResponse OnDeath()
+        {
+            return LoseItem();
+        }
+    }
+}
