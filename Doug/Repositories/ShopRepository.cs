210edf3f58e0798f11eaf09878979df75479bb8b
Hugues Trottier <hugues_trottier@hotmail.com>
2019-07-14 18:38:10 -0400

    shop repository oops

---
+++ Doug/Repositories/ShopRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Doug.Models;
+
+namespace Doug.Repositories
+{
+    public interface IShopRepository
+    {
+        Shop GetShop(string shopId);
+        Shop GetDefaultShop();
+    }
+
+    public class ShopRepository : IShopRepository
+    {
+        public const string DefaultShopId = "default";
+
+        private readonly DougContext _db;
+
+        public ShopRepository(DougContext db)
+        {
+            _db = db;
+        }
+
+        public Shop GetShop(string shopId)
+        {
+            return _db.Shops.SingleOrDefault(shop => shop.Id == shopId);
+        }
+
+        public Shop GetDefaultShop()
+        {
+            return _db.Shops.Single(shop => shop.Id == DefaultShopId);
+        }
+    }
+}
