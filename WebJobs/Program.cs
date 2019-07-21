e8e1f828c8575f26ba66740fab7f19919c5ea095
Hugues Trottier <hugues_trottier@hotmail.com>
2019-07-21 18:15:22 -0400

    ruler get 100 rupee per day

--- WebJobs/Program.cs
+++ WebJobs/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System;
+using System.Linq;
+using Doug.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebJobs
 {
@@ -8,15 +11,30 @@ namespace WebJobs
     {
         static void Main()
         {
-            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dougbotdb"].ConnectionString))
-            using (var command = new SqlCommand("DailyReset", conn)
-            {
-                CommandType = CommandType.StoredProcedure
-            })
-            {
-                conn.Open();
-                command.ExecuteNonQuery();
-            }
+            var connectionString = Environment.GetEnvironmentVariable("dougbotdb");
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddDbContext<DougContext>(options => options.UseSqlServer(connectionString))
+                .BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+            logger.LogDebug("Starting daily reset");
+
+            var db = serviceProvider.GetService<DougContext>();
+            AddCreditsToRuler(db);
+
+
+            db.SaveChanges();
+            logger.LogDebug("All done!");
+        }
+
+        private static void AddCreditsToRuler(DougContext db)
+        {
+            var government = db.Government.Single();
+
+            var ruler = db.Users.Single(usr => usr.Id == government.Ruler);
+            ruler.Credits += 100;
         }
     }
 }
