1818affe2888af61415b7a49f15cbae4c17e34ec
WGF <girouxfortinw@hotmail.com>
2019-07-31 18:49:50 -0400

    Create IncognitoShades.cs

---
+++ Doug/Items/Equipment/IncognitoShades.cs
@@ -0,0 +1,36 @@
+﻿using Doug.Models;
+using Doug.Services;
+using Doug.Slack;
+
+namespace Doug.Items.Equipment
+{
+    public class IncognitoShades : EquipmentItem
+    {
+        public const string ItemId = "incognito_shades";
+
+        private readonly ISlackWebApi _slack;
+        private readonly IUserService _userService;
+
+        public IncognitoShades(ISlackWebApi slack, IUserService userService)
+        {
+            _slack = slack;
+            _userService = userService;
+
+            Id = ItemId;
+            Name = "Incognito Shades";
+            Description = "Really sick shades, you feel like wearing them even at night. +4 Luck, and you'll look too cool for Doug to recognize you.";
+            Rarity = Rarity.Rare;
+            Icon = ":dark_sunglasses:";
+            Slot = EquipmentSlot.Neck;
+            Price = 2600;
+            LevelRequirement = 10;
+
+            Stats.Luck = 4;
+        }
+
+        public override string OnMention(string mention)
+        {
+            return ":sunglasses:";
+        }
+    }
+}
