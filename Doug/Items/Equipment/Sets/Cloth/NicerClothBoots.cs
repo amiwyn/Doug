72c8344f518cd8cfdf861485ad45b5ab21dcb0d6
lluren <foum94@gmail.com>
2019-07-31 13:00:05 -0400

    Create NicerClothBoots.cs

---
+++ Doug/Items/Equipment/Sets/Cloth/NicerClothBoots.cs
@@ -0,0 +1,24 @@
+namespace Doug.Items.Equipment.Sets.Cloth
+{
+    public class NicerClothBoots : EquipmentItem
+    {
+        public const string ItemId = "nicer_cloth_boots";
+
+        public NicerClothBoots()
+        {
+            Id = ItemId;
+            Name = "Nicer Cloth Boots";
+            Description = "These boots are made of nicer cloth.";
+            Rarity = Rarity.Common;
+            Icon = ":boots_1:";
+            Slot = EquipmentSlot.Boots;
+            Price = 460;
+            LevelRequirement = 15;
+            IntelligenceRequirement = 20;
+
+            Stats.Defense = 10;
+            Stats.Dodge = 8;
+            Stats.Energy = 30;
+        }
+    }
+}
