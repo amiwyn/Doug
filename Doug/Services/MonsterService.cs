6a06afb1c34c260af366eaabaa13afc114b9b9e3
Hugues Trottier <hugues_trottier@hotmail.com>
2019-08-01 19:09:03 -0400

    moved spawn back into services

--- Doug/Services/MonsterService.cs
+++ Doug/Services/MonsterService.cs
@@ -10,24 +10,66 @@ namespace Doug.Services
 {
     public interface IMonsterService
     {
+        void RollMonsterSpawn();
         Task HandleMonsterDeathByUser(User user, SpawnedMonster spawnedMonster, string channel);
     }
 
     public class MonsterService : IMonsterService
     {
+        private const double SpawnChance = 0.2;
+        private const string PvpChannel = "CL2TYGE1E";
+        private const int MaximumMonsterTypeInChannel = 3;
+
         private readonly IMonsterRepository _monsterRepository;
         private readonly ISlackWebApi _slack;
         private readonly IUserService _userService;
         private readonly IInventoryRepository _inventoryRepository;
         private readonly IRandomService _randomService;
+        private readonly IMonsterFactory _monsterFactory;
+        private readonly IChannelRepository _channelRepository;
 
-        public MonsterService(IMonsterRepository monsterRepository, ISlackWebApi slack, IUserService userService, IInventoryRepository inventoryRepository, IRandomService randomService)
+        public MonsterService(IMonsterRepository monsterRepository, ISlackWebApi slack, IUserService userService, IInventoryRepository inventoryRepository, IRandomService randomService, IMonsterFactory monsterFactory, IChannelRepository channelRepository)
         {
             _monsterRepository = monsterRepository;
             _slack = slack;
             _userService = userService;
             _inventoryRepository = inventoryRepository;
             _randomService = randomService;
+            _monsterFactory = monsterFactory;
+            _channelRepository = channelRepository;
+        }
+
+        public void RollMonsterSpawn()
+        {
+            var random = new Random();
+            if (random.NextDouble() >= SpawnChance)
+            {
+                return;
+            }
+
+            var channel = PvpChannel;
+            if (random.Next(2) == 0)
+            {
+                channel = PickRandomChannel(random, _channelRepository).Id;
+            }
+
+            var monster = _monsterFactory.CreateRandomMonster(random);
+            var monstersInChannel = _monsterRepository.GetMonsters(channel);
+
+            if (monstersInChannel.Count(monsta => monsta.MonsterId == monster.Id) >= MaximumMonsterTypeInChannel)
+            {
+                return;
+            }
+
+            _monsterRepository.SpawnMonster(monster, channel);
+            _slack.BroadcastMessage(string.Format(DougMessages.MonsterSpawned, monster.Name), channel).Wait();
+        }
+
+        private Channel PickRandomChannel(Random random, IChannelRepository channelRepository)
+        {
+            var channels = channelRepository.GetChannels().ToList();
+            var index = random.Next(0, channels.Count);
+            return channels.ElementAt(index);
         }
 
         public async Task HandleMonsterDeathByUser(User user, SpawnedMonster spawnedMonster, string channel)
--- MonsterSpawnWebJob/Program.cs
+++ MonsterSpawnWebJob/Program.cs
@@ -15,10 +15,6 @@ namespace MonsterSpawnWebJob
 {
     class Program
     {
-        private const double SpawnChance = 0.2;
-        private const string PvpChannel = "CL2TYGE1E";
-        private const int MaximumMonsterTypeInChannel = 3;
-
         static void Main()
         {
             var connectionString = Environment.GetEnvironmentVariable("dougbotdb");
@@ -30,46 +26,9 @@ namespace MonsterSpawnWebJob
             Startup.RegisterDougServices(services);
             var serviceProvider = services.BuildServiceProvider();
 
-            var channelRepository = serviceProvider.GetService<IChannelRepository>();
-            var monsterFactory = serviceProvider.GetService<IMonsterFactory>();
-            var monsterRepository = serviceProvider.GetService<IMonsterRepository>();
-            var slack = serviceProvider.GetService<ISlackWebApi>();
+            var monsterService = serviceProvider.GetService<IMonsterService>();
 
-            RollMonsterSpawn(channelRepository, monsterFactory, monsterRepository, slack);
-        }
-
-        private static void RollMonsterSpawn(IChannelRepository channelRepository, IMonsterFactory monsterFactory,
-            IMonsterRepository monsterRepository, ISlackWebApi slack)
-        {
-            var random = new Random();
-            if (random.NextDouble() >= SpawnChance)
-            {
-                return;
-            }
-
-            var channel = PvpChannel;
-            if (random.Next(2) == 0)
-            {
-                channel = PickRandomChannel(random, channelRepository).Id;
-            }
-
-            var monster = monsterFactory.CreateRandomMonster(random);
-            var monstersInChannel = monsterRepository.GetMonsters(channel);
-
-            if (monstersInChannel.Count(monsta => monsta.MonsterId == monster.Id) >= MaximumMonsterTypeInChannel)
-            {
-                return;
-            }
-
-            monsterRepository.SpawnMonster(monster, channel);
-            slack.BroadcastMessage(string.Format(DougMessages.MonsterSpawned, monster.Name), channel).Wait();
-        }
-
-        private static Channel PickRandomChannel(Random random, IChannelRepository channelRepository)
-        {
-            var channels = channelRepository.GetChannels().ToList();
-            var index = random.Next(0, channels.Count);
-            return channels.ElementAt(index);
+            monsterService.RollMonsterSpawn();
         }
     }
 }
