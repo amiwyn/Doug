8f38d416802e706f72590daee57952d0ce1dc7b3
Hugues Trottier <hugues_trottier@hotmail.com>
2019-11-11 19:08:33 -0500

    ingots

---
+++ Doug/Items/Misc/Drops/MithrilIngot.cs
@@ -0,0 +1,17 @@
+﻿namespace Doug.Items.Misc.Drops
+{
+    public class MithrilIngot : Item
+    {
+        public const string ItemId = "mithril_ingot";
+        public MithrilIngot()
+        {
+            Id = ItemId;
+            Name = "Mithril Ingot";
+            Description = "An ingot made of mithril metal.";
+            Rarity = Rarity.Uncommon;
+            Icon = ":mithril_ingot:";
+            Price = 2880;
+            MaxStack = 99;
+        }
+    }
+}
