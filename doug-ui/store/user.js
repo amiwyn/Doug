384ef39d4b3ae0ccfe1c6fa37a1cdfe84b2acbfd
Hugues Trottier <hugues_trottier@hotmail.com>
2019-12-16 07:26:10 -0500

    fixed dougui urls

--- doug-ui/store/user.js
+++ doug-ui/store/user.js
@@ -4,28 +4,29 @@ export const state = () => ({
 
 export const actions = {
   async equip(context, position) {
-    var result = await this.$axios.$post(`https://0b2bd38f.ngrok.io/ui/user/equip/${position}`)
+    console.log(this.$axios)
+    var result = await this.$axios.$post(`https://doug-bot.azurewebsites.net/ui/user/equip/${position}`)
 
     context.commit('setInventory', result.items, { root: true })
     context.commit('setLoadout', result.loadout, { root: true })
     return result.action_message
   },
   async unequip(context, slot) {
-    var result = await this.$axios.$post(`https://0b2bd38f.ngrok.io/ui/user/unequip/${slot}`)
+    var result = await this.$axios.$post(`https://doug-bot.azurewebsites.net/ui/user/unequip/${slot}`)
     
     context.commit('setInventory', result.items, { root: true })
     context.commit('setLoadout', result.loadout, { root: true })
     return result.action_message
   },
   async use(context, position) {
-    var result = await this.$axios.$post(`https://0b2bd38f.ngrok.io/ui/user/use/${position}`)
+    var result = await this.$axios.$post(`https://doug-bot.azurewebsites.net/ui/user/use/${position}`)
     
     context.commit('setInventory', result.items, { root: true })
     context.commit('setLoadout', result.loadout, { root: true })
     return result.action_message
   },
   async sell(context, position) {
-    var result = await this.$axios.$post(`https://0b2bd38f.ngrok.io/ui/user/sell/${position}`)
+    var result = await this.$axios.$post(`https://doug-bot.azurewebsites.net/ui/user/sell/${position}`)
     
     context.commit('setInventory', result.items, { root: true })
     context.commit('setLoadout', result.loadout, { root: true })
